0e60bf11cd0d74f4f47e23c70f3e49c68615f3d7
shai sandhiya <shaisandhiya5566@gmail.com>
2026-08-12 21:04:46 +0530

    Add factorial calculation in factorial.java

---
+++ factorial.java
@@ -0,0 +1,13 @@
+class factorial{
+	public static void main(String[]args) {
+		int n=5;
+		int i;
+		int fact=1;
+		for(i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		System.out.println(fact);
+			
+			
+		}
+	}
